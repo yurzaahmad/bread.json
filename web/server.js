aae7cfcab638144c86019034fe6a28340cc4c6bb
yurza <ahmadyurza@gmail.com>
2020-08-14 21:24:00 +0700

    daah

--- web/server.js
+++ web/server.js
@@ -4,13 +4,10 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { text } = require('body-parser');
 const dataSiswa = JSON.parse(fs.readFileSync("data.json", "utf8"))
-// const obj = JSON.parse(text)
+
 
 
 let data = []
-// let i
-
-// const dataBase = JSON.parse(fs.readFileSync("data.json", "utf8"))
 
 const app = express()
 
@@ -26,8 +23,7 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    // console.log(dataSiswa)
-    // console.log(dataSiswa)
+
     res.render('list', { dataSiswa })
 })
 
@@ -44,12 +40,7 @@ app.get('/delete/:id', (req, res) => {
     dataSiswa.splice(req.params.id, 1)
     res.redirect('/')
 })
-// app.get("/edit", (req, res) => {
 
-//     const{string,integer,float,date,boolean} =dataSiswa[i]
-//     data = dataSiswa[i]
-//     res.render('list', { strEjs:string, integerEjs:integer,floatEjs:float, dateEjs:date, booleanEjs:boolean, index:i })
-// })
 
 app.get("/edit/:id", (req, res) => {
     let id = req.params.id;
@@ -71,13 +62,11 @@ app.post("/edit/:id", (req, res) => {
     console.log(req.body)
     dataSiswa.splice(id, 1, edit)
     fs.writeFileSync("data.json", JSON.stringify(dataSiswa))
-    // dataSiswa.push((i), 1,req.body);
-    // dataSiswa.splice((id),1,req.body)
-    // dataSiswa.splice(req.params.id, 1)
+
     res.redirect("/")
 })
 
-
-app.listen(3000, () => {
-    console.log(`web ini berjalan di port 3000!`)
+let port = 4000
+app.listen(port, () => {
+    console.log(`web ini berjalan di port ${port}!`)
 })
